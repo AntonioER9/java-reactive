bc4f32de1420f63bff825c29bb410dcdca40f310
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-08-21 09:19:22 -0400

    Refactor ReactiveIntro to replace courseStream with names Observable and handle errors

--- src/main/java/com/antonio/reactive/ReactiveIntro.java
+++ src/main/java/com/antonio/reactive/ReactiveIntro.java
@@ -4,12 +4,26 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class ReactiveIntro {
     public static void main(String[] args) {
-        Observable<String> courseStream = Observable.just("HTML","CSS","JAVA", "C");
+//        Observable<String> courseStream = Observable.just("HTML","CSS","JAVA", "C");
+//
+//        courseStream.subscribe(
+//            item -> System.out.println("Recibido " + item),
+//            error -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Finalizando...")
+//        );
 
-        courseStream.subscribe(
+        Observable<String> names = Observable.create( emitter -> {
+            emitter.onNext("Fernando");
+            emitter.onNext("Antonio");
+            emitter.onNext("Juan");
+            emitter.onError(new IllegalStateException("Error"));
+        });
+
+        names.subscribe(
             item -> System.out.println("Recibido " + item),
             error -> System.out.println(error.getMessage()),
-                () -> System.out.println("Finalizando...")
+            () -> System.out.println("Finalizando...")
         );
+
     }
 }
